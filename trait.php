0b5f69487f88afaafb4fff427a792d29c1517889
ruman649 <mruman649@gmail.com>
2022-11-24 15:27:10 +0600

    new trait added

--- trait.php
+++ trait.php
@@ -5,10 +5,18 @@ trait name{
         echo "Hello Bangladesh";
     }
 }
+trait age{
+    public function myAge(){
+        echo "My Age is ". 67;
+    }
+}
 
 class student{
     use name;
+    use age;
 }
 
 $obj = new student();
 $obj->Greating();
+echo "<br>";
+$obj->myAge();
